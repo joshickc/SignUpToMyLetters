4f6e8448a5594660be1c2f5849d33795fa07283a
Jayant Joshi <jayantjoshickc@gmail.com>
2023-06-12 14:05:05 +0530

    form submission

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/signup.html");
+    console.log("website open");
+});
+
+app.post("/", function(req, res){
+    console.log("reached post");
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    console.log(req.body.email);
+    res.sendFile(__dirname + "/success.html");
+
+});
+
 app.listen(3000, function () {
     console.log("server is running");
-})
+});
